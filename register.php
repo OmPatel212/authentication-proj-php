674d7b7993ad98e09a053e01d02a3543d09b043d
OM <omfaldu1406@gmail.com>
2026-08-17 17:08:07 +0530

    TG-2 TG-3 #done

---
+++ register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign Up</title>
+</head>
+
+<body>
+
+<h2>Create Account</h2>
+
+<form method="POST">
+
+    <input type="text" name="name" placeholder="Name">
+    <br><br>
+
+    <input type="email" name="email" placeholder="Email">
+    <br><br>
+
+    <input type="password" name="password" placeholder="Password">
+    <br><br>
+
+    <button type="submit">
+        Sign Up
+    </button>
+
+</form>
+
+</body>
+</html>
